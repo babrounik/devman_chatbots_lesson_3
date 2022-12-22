1e007f38c9873e5c551f3d6bfc4a7bf9b2378a55
Artsiom Babrounik <a.bobrovnik@purple.games>
2022-12-22 15:48:45 +0400

    setup DialogFlow

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import logging
 import os
 
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -41,7 +42,45 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+
+    project_id = os.getenv("DIALOGFLOW_PROJECT")
+    session_id = os.getenv("ARTSIOM_CHAT_ID")
+    texts = update.message.text
+    language_code = "RU"
+    response = detect_intent_texts(project_id, session_id, texts, language_code)
+
+    update.message.reply_text(response)
+    # update.message.reply_text(update.message.text)
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+    return response.query_result.fulfillment_text
 
 
 def main() -> None:
